2bee7eea44dbe2e4d5203aad2010499f3e48b2b1
Manviel <Mihaylo.Merezhko@kname.edu.ua>
2017-09-30 18:31:47 +0300

    The power of Components

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import './Style.css';
 
 import Post from './Post';
@@ -7,18 +6,7 @@ import Post from './Post';
 class Container extends Component {
   render() {
     return(
-      <div>
-        <div className="line">
-          <Link to="/profile">
-            <img src={require("../img/profile.jpg")} alt="profile" className="icon" />
-          </Link>
-          <div className="nick">
-            <p className="bold">developer</p>
-          </div>
-          <img src={require("../img/dot.png")} alt="dot" className="dot" />
-        </div>
       <Post />
-      </div>
     );
   }
 }
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import './Style.css';
+
+import preload from '../data/data.json';
 
 class Post extends Component {
   render() {
     return(
-      <div className="picture">
-        <img src={require("../img/pic.jpg")} alt="pic" id="pic" />
-      </div>
+      <section className="post">
+        {preload.photos.map((act) => {
+          return(
+            <div className="item">
+              <article className="line">
+                <Link to="/profile">
+                  <img src={require("../img/profile.jpg")} alt="profile" className="icon" />
+                </Link>
+                <div className="nick">
+                  <p className="bold">developer</p>
+                </div>
+                <img src={require("../img/dot.png")} alt="dot" className="dot" />
+              </article>
+              <article className="picture">
+                <img src={require(`../img/${act.src}`)} alt={`${act.id}`} id="pic" />
+              </article>
+            </div>
+          );
+        })}
+      </section>
     );
   }
 }
