486a1eb8ab55b024ba2af6a15a1da3ec1579b900
Manviel <Mihaylo.Merezhko@kname.edu.ua>
2017-10-02 18:34:54 +0300

    counter handleClick

--- src/components/Description.js
+++ src/components/Description.js
@@ -1,29 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './css/Beaty.css';
 
-const Description = (props) => {
-  return(
-    <section className="wrapper">
-      <div className="description">
-        <button id="like">
-          <div className="heart"></div>
-        </button>
-        <button id="comment">
-          <div className="chat"></div>
-        </button>
-        <p className="likes">
-          <span id="counter">0</span> likes
-        </p>
-        <p>
-          <span className="bold">developer</span>: {props.show.description}
-        </p>
-      </div>
-      <div className="comments">
-        <input type="text" name="message" placeholder="Add a comment" id="input" />
-        <ul id="messages"></ul>
-      </div>
-    </section>
-  );
+class Description extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {clicks: 0}
+  }
+
+  handleClick() {
+    this.setState((prevState) => ({
+      clicks: prevState.clicks + 1
+    }));
+  }
+
+  render() {
+    return(
+      <section className="wrapper">
+        <div className="description">
+          <button id="like" onClick={this.handleClick.bind(this)}>
+            <div className="heart"></div>
+          </button>
+          <button id="comment">
+            <div className="chat"></div>
+          </button>
+          <p className="likes">
+            <span id="counter">{this.state.clicks}</span> likes
+          </p>
+          <p>
+            <span className="bold">developer</span>: {this.props.show.description}
+          </p>
+        </div>
+        <div className="comments">
+          <input type="text" name="message" placeholder="Add a comment" id="input" />
+          <ul id="messages"></ul>
+        </div>
+      </section>
+    );
+  }
 }
 
 export default Description;
