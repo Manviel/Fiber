39bbebbed67dd87a9225c15cc59a6c1a8b344c33
Manviel <Mihaylo.Merezhko@kname.edu.ua>
2017-10-05 21:28:18 +0300

    Add PopUp to dot

---
+++ src/components/Popup.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Popup = (props) => {
+  return(
+    <div className="popup">
+      <div className="inner">
+        <button className="option"
+          onClick={props.closePop}>Cancel</button>
+      </div>
+    </div>
+  );
+}
+
+export default Popup;
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,24 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './css/Style.css';
 
-const Post = (props) => {
-  return(
-    <div className="item">
-      <article className="line">
-        <Link to="/profile">
-          <img src={require("../img/profile.jpg")} alt="profile" className="icon" />
-          <div className="nick">
-            <p className="bold">developer</p>
-          </div>
+import Popup from './Popup';
+
+class Post extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showPop: false
+    };
+  }
+
+  togglePop() {
+    this.setState({
+      showPop: !this.state.showPop
+    });
+  }
+
+  render() {
+    return(
+      <div className="item">
+        <article className="line">
+          <Link to="/profile">
+            <img src={require("../img/profile.jpg")} alt="profile" className="icon" />
+            <div className="nick">
+              <p className="bold">developer</p>
+            </div>
+          </Link>
+          <img src={require("../img/dot.png")} alt="dot"
+            className="dot" onClick={this.togglePop.bind(this)} />
+          {this.state.showPop ?
+            <Popup
+              closePop={this.togglePop.bind(this)} /> : null}
+        </article>
+        <Link className="picture" to={`/details/${this.props.id}`}>
+          <img src={require(`../img/${this.props.src}`)} alt={`${this.props.id}`} id="pic" />
         </Link>
-        <img src={require("../img/dot.png")} alt="dot" className="dot" />
-      </article>
-      <Link className="picture" to={`/details/${props.id}`}>
-        <img src={require(`../img/${props.src}`)} alt={`${props.id}`} id="pic" />
-      </Link>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default Post;
