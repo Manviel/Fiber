cb04ec472875e594cafc0dde5cb284f37f2c4c41
Manviel <Mihaylo.Merezhko@kname.edu.ua>
2018-05-09 22:12:21 +0300

    Potential security vulnerability

--- src/components/editor/Image.js
+++ src/components/editor/Image.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Image(props) {
   return (
     <article className="picture">
-      <img src={props.image} alt="preview" className="pic" />
+      <img src={props.image} alt="preview" className="auto" />
     </article>
   );
 }
